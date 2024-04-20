3a17926ed6158479f8bf62a140da2cdf2c0a999a
bwadley12 <brad.willimack@gmail.com>
2024-04-19 23:44:36 -0600

    added pounds to routes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,10 +11,10 @@ function NavEntry({title, location}) {
 export default function NavBar() {
     return (
         <div className="navbar">
-            <NavEntry title="HOME" location="/" />
-            <NavEntry title="PROJECTS" location="/projects" />
-            <NavEntry title="ABOUT" location ="/about"/>
-            <NavEntry title="3D PRINTING" location ="/threedprinting"/>
+            <NavEntry title="HOME" location="#/" />
+            <NavEntry title="PROJECTS" location="#/projects" />
+            <NavEntry title="ABOUT" location ="#/about"/>
+            <NavEntry title="3D PRINTING" location ="#/threedprinting"/>
         </div>
     )
 }
