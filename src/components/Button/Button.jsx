09f86e76585b5eb6445369cf919d82ea8b1c429f
Willimack <Bradley.Willimack@jacobs.com>
2024-04-25 21:26:24 -0600

    Updates

--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import "./Button.css";
-
-export default function Button() {
-    return (
-        <button type="button" className="myButton">button </button>
-    )
-}
--- src/routes/ErrorPage/ErrorPage.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function ErrorPage() {
-    return (
-        <h1>Uh oh, error page unfortunately</h1>
-    )
-}
